80f6eeb1fe3966cdde39f1c2248da0448ceb42b1
Dom Goodwin <d0m182goodwin@gmail.com>
2017-04-16 18:04:39 +0100

    Added express package and changed index

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-// content of index.js
-const http = require('http')  
-const port = 3000
+var express = require('express')
+var app = express()
 
-const requestHandler = (request, response) => {  
-  console.log(request.url)
-  response.end('<html><head></head><body><h1>Hello Node.js Server! BlokPayments - Coming Soon!</h1><h2>test</h2></body></html>')
-}
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
 
-const server = http.createServer(requestHandler)
-
-server.listen(port, (err) => {  
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-
-  console.log(`server is listening on ${port}`)
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
 })
